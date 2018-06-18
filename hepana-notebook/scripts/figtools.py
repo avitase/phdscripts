e504e21fb75e34e11a31585137aad38dd3598eb1
Nis Meinert <nis.meinert@gmail.com>
2018-06-18 14:59:54 +0200

    fix broken cache fooling

--- hepana-notebook/scripts/figtools.py
+++ hepana-notebook/scripts/figtools.py
@@ -2,9 +2,11 @@ import time
 import os
 from IPython.display import display, HTML
 
-def show_fig(filename, directory='img', width=400, fool_caching=True):
-    html_template = '<img src="{0}" alt="{0}" width="{2}">'
-    rnd = str(time.time()) if fool_caching else ''
-    fullname = os.path.join(directory, filename)
-    display(HTML(html_template.format(fullname, '?' + rnd, width)))
+def show_fig(file_name, directory='img', width=400, fool_caching=True):
+    html_template = '<img src="{0}" alt="{1}" width="{2}">'
+    full_name = os.path.join(directory, file_name)
+    if fool_caching:
+        rnd = str(time.time())
+        full_name += '?' + rnd
+    display(HTML(html_template.format(full_name, file_name, width)))
 
