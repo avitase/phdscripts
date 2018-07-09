a92f35d112dde3399b1cde5dd1b0ac88d3899145
Nis Meinert <nis.meinert@gmail.com>
2018-07-09 11:22:45 +0200

    add dump_hist_with_given_uncertainties

--- hepana-notebook/scripts/histdump.py
+++ hepana-notebook/scripts/histdump.py
@@ -7,7 +7,9 @@ def poisson_interval(k, alpha=2.*0.159):
     low = chi2.ppf(alpha/2., 2*k) / 2. if k > 0 else 0.
     return low, high
 
-def dump_hist(hist, bin_edges, file_name, error_fct=poisson_interval):
+def dump_hist(hist, bin_edges, file_name, error_fct=poisson_interval, errors=[]):
+    assert len(errors) == 0 or len(errors) == len(hist)
+
     file_name = os.path.join('img', file_name)
 
     columns = ['lower bin edge', 'bin center', 'upper bin edge',
@@ -20,16 +22,25 @@ def dump_hist(hist, bin_edges, file_name, error_fct=poisson_interval):
         for n in hist:
             bin_edge = bin_edges[i], bin_edges[i+1]
             bin_center = np.mean(bin_edge)
-            i = i + 1
 
-            error_low, error_up = error_fct(n) if error_fct else (0., 0.)
+            if errors: 
+                error_low, error_up = n - errors[i], n + errors[i]
+            else:
+                error_low, error_up = error_fct(n)
+
+            i = i + 1
 
             values = bin_edge[0], bin_center, bin_edge[1], error_low, n, error_up
             f.write(row_temp.format(*values) + '\n')
 
-def make_hist_file(data, bin_edges, file_name, error_fct=poisson_interval):
+def make_hist_file(data, bin_edges, file_name, error_fct=poisson_interval, errors=[]):
     hist, bin_edges = np.histogram(data, bin_edges)
-    dump_hist(hist, bin_edges, file_name, error_fct)
+    dump_hist(hist, bin_edges, file_name, error_fct, errors)
+
+def dump_hist_with_given_uncertainties(data, bin_edges, file_name):
+    data = [(float(x.n), float(x.s)) for x in data]
+    data, errors = zip(*data)
+    dump_hist(data, bin_edges, file_name, errors=errors)
 
 def make_steps_file(func, bin_edges, file_name, n=100):
     file_name = os.path.join('img', file_name)
