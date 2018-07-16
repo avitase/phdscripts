f8bdbab1ec9102ea28e800d927ac6ad0e6736725
Nis Meinert <nis.meinert@gmail.com>
2018-07-16 18:29:45 +0200

    add integral

--- hepana-notebook/scripts/pdfs.py
+++ hepana-notebook/scripts/pdfs.py
@@ -3,11 +3,17 @@ from math import sqrt, pi, erf, exp
 class Linear:
     def __init__(self, boundaries):
         self.boundaries = boundaries
+
+    def _norm(a):
+        l, r = self.boundaries
+        return a / 2. * (r**2 - l**2) + r - l
     
     def __call__(self, x, a):
-        l, r = self.boundaries
-        norm = a / 2. * (r*r - l*l) + r - l
-        return (1. + a * x) / norm
+        return (1. + a * x) / _norm(a)
+
+    def integral(x):
+        return (x + a / 2. * x**2) / _norm(a)
+
 
 class Gaussian:
     SQRT2 = sqrt(2.)
@@ -15,13 +21,18 @@ class Gaussian:
     
     def __init__(self, boundaries):
         self.boundaries = boundaries
+
+    def _t(x):
+        return (x - mean) / self.SQRT2 / width
     
     def __call__(self, x, mean, width):
-        t = lambda x: (x - mean) / self.SQRT2 / width
-        
         l, r = self.boundaries
         norm = self.SQRTPI / self.SQRT2 * width
-        norm *= erf(t(r)) - erf(t(l))
+        norm *= erf(_t(r)) - erf(_t(l))
         
-        tx = t(x)
+        tx = _t(x)
         return exp(-tx * tx) / norm
+
+    def integral(x):
+        l, r = self.boundaries
+        return erf(_t(x)) / (erf(_t(r)) - erf(_t(l)))
