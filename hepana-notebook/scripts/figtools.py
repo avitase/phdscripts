ac66df2fdf8d29dbc547070e4c6a41e17c4eb24d
Nis Meinert <nis.meinert@gmail.com>
2018-10-16 14:54:32 +0200

    add global variable never_fool_caching

--- hepana-notebook/scripts/figtools.py
+++ hepana-notebook/scripts/figtools.py
@@ -2,11 +2,13 @@ import time
 import os
 from IPython.display import display, HTML
 
+never_fool_caching = False
+
 def show_fig(file_name, directory='img', width=400, fool_caching=True):
     html_template = '<img src="{0}" alt="{1}" width="{2}">'
     full_name = os.path.join(directory, file_name)
-    if fool_caching:
-        rnd = str(time.time())
-        full_name += '?' + rnd
+    if fool_caching and not never_fool_caching:
+        rnd = time.time()
+        full_name += '?{:.0f}'.format(rnd)
     display(HTML(html_template.format(full_name, file_name, width)))
 
